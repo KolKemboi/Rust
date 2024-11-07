4dec5e8fe2a869af9f3fd83407d40878f679e221
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2024-11-07 13:17:58 +0300

    update

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,12 +1,114 @@
 #[allow(unused)]
 
+use std::collections::HashMap;
+use std::hash::Hash;
+
 fn main()
 {
     let data = Data::new("info", "endpoint");
     data.ret_endpoint();
     data.ret_info();
+
+    let mut stack = Stack::new();
+    println!("{}", stack.is_empty());
+
+    stack.push(10);
+    stack.push(1);
+    stack.push(0);
+    stack.push(110);
+    
+    println!("{:?}", stack.items);
+
+    match stack.pop() {
+        Some(val) =>println!("{}", val),
+        None => println!("Is empty"),
+    }
+
+    match stack.peek()
+    {
+        Some(val) => println!("{}", val),
+        None => println!("Nothing to peak"),
+    }
+
+    let mut hashi = Container::<String, i32>::new();
+    hashi.add_value(String::from("v_1"), 10);
+    hashi.add_value(String::from("v_2"), 11);
+    hashi.add_value(String::from("v_3"), 12);
+    hashi.add_value(String::from("v_4"), 13);
+
+    let key = String::from("v_2");
+    match hashi.get_value(&key)
+    {
+        Some(value) =>println!("{}", value),
+        None => println!("No Key or value"),
+    }
+
 }
 
+struct Container<K, V>
+where
+    K: Eq + Hash
+{
+    items: HashMap<K, V>,
+}
+
+impl<K, V> Container<K, V>
+where
+    K: Eq + Hash
+{
+    fn new() -> Self {
+        Container {
+            items: HashMap::new(),
+        }
+    }
+
+    fn add_value(&mut self, key: K, value: V) {
+        self.items.insert(key, value);
+    }
+
+    fn get_value(&mut self, key: &K) ->Option<&V>
+    {
+        self.items.get(key)
+    }
+
+}
+
+
+struct Stack<T>
+{
+    items:Vec<T>,
+}
+
+impl<T> Stack<T>
+{
+    fn new() ->Self
+    {
+        Stack { items: Vec::new() }
+    }
+
+    fn push(&mut self, item: T)
+    {
+        self.items.push(item);
+    }
+
+    fn pop(&mut self) -> Option<T>
+    {
+        self.items.pop()
+    }
+
+    fn peek(&mut self) ->Option<&T>
+    {
+        self.items.last()
+    }
+
+    fn is_empty(&mut self) ->bool
+    {
+        self.items.is_empty()
+    }
+
+}
+
+#[warn(dead_code)]
 trait InfoShow
 {
     fn showinfo(&self) ->String;
